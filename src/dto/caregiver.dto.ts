0f5ce6f0142501f2285189719ac7e1eaceac1402
Raphael Ramalho de Souza <raphaelramalho41@gmail.com>
2025-05-07 15:58:17 -0300

    segunda Atualização do DTO de Caregiver

--- src/dto/caregiver.dto.ts
+++ src/dto/caregiver.dto.ts
@@ -11,7 +11,9 @@ export class CreateCaregiverDto {
 
   @IsNotEmpty()
   @IsString()
-  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number or special character',
+  })
   password: string;
 }
 
